b9c22152e56903c5137c50f85b375ab54481d76c
nlouwen <nicolouwen@hotmail.com>
2024-03-05 15:11:06 +0100

    add temp data saving

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import (
     Flask,
     render_template,
+    render_template_string,
     request,
     redirect,
     url_for,
@@ -19,6 +20,9 @@ from rdkit import Chem, rdBase
 from rdkit.Chem import Draw, rdDepictor
 from rdkit.Chem.Draw import rdMolDraw2D
 from werkzeug.datastructures import MultiDict
+from pathlib import Path
+import json
+import csv
 
 app = Flask(__name__)
 app.secret_key = "IYKYK"
@@ -27,9 +31,18 @@ app.secret_key = "IYKYK"
 ### Landing page
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """Main page, edit existing entry or start new entry"""
     form = SelectExisting(request.form)
-    if request.method == "POST" and form.validate():
-        return redirect(url_for("edit_bgc", bgc_id=form.accession.data))
+    if request.method == "POST":
+        # create new entry
+        if form.submit.data:
+            bgc_id = create_new_entry()
+            return redirect(url_for("edit_bgc", bgc_id=bgc_id))
+
+        # edit valid existing entry
+        if form.edit.data and form.validate():
+            return redirect(url_for("edit_bgc", bgc_id=form.accession.data))
+
     return render_template("index.html", form=form)
 
 
@@ -60,28 +73,17 @@ def edit_minimal(bgc_id: str):
     if not is_valid_bgc_id(bgc_id):
         return "Invalid existing entry!", 404
 
+    # try to fill data from existing entry
     if not request.form:
-        form = MinEntryForm(
-            MultiDict(
-                {
-                    "genome": "ABG100001.1",
-                    "location-start": 1,
-                    "location-end": 4,
-                    "products": ["FAKEOMYCIN"],
-                }
-            )
-        )
+        form = MinEntryForm(MultiDict(read_data(bgc_id).get("Minimal")))
     else:
         form = MinEntryForm(request.form)
-    if request.method == "POST":
-        if form.add_evidence.data:
-            form.evidence.append_entry()
-            return render_template("min_entry.html", form=form, bgc_id=bgc_id)
 
-        if form.validate():
-            # return form.data  # TODO: save to db
-            flash("Submitted minimal entry!")
-            return redirect(url_for("edit_bgc", bgc_id=bgc_id))
+    if request.method == "POST" and form.validate():
+        # TODO: save to database
+        save_data(bgc_id, "Minimal", request.form)
+        flash("Submitted minimal entry!")
+        return redirect(url_for("edit_bgc", bgc_id=bgc_id))
     return render_template("min_entry.html", form=form, bgc_id=bgc_id)
 
 
@@ -99,19 +101,25 @@ def edit_structure(bgc_id: str):
             return render_template("structure.html", form=form, bgc_id=bgc_id)
         elif form.submit.data and form.validate():
             # return form.data  # TODO: save to db
+            save_data(bgc_id, "Structure", request.form)
             flash("Submitted structure information!")
             return redirect(url_for("edit_bgc", bgc_id=bgc_id))
 
     # on GET query db for any products already present
     else:
         # if one (or more) is present prefill
-        for c_name, smiles in [
-            (
-                "EXAMPLEMYCIN",
-                "C[C@@H](CCC=C(C)C)[C@@H]1CC[C@]2([C@]1(CC[C@H]3C2=CC[C@@H]4[C@@]3(CC[C@@H](C4(C)C)O)C)C)C",
+        try:
+            products = next(
+                csv.reader(
+                    [MultiDict(read_data(bgc_id).get("Minimal")).get("products")],
+                    skipinitialspace=True,
+                )
             )
-        ]:
-            form.structures.append_entry(data={"name": c_name, "structure": smiles})
+        except:
+            products = [""]
+
+        for product in products:
+            form.structures.append_entry(data={"name": product})
     return render_template("structure.html", form=form, bgc_id=bgc_id)
 
 
@@ -157,12 +165,23 @@ def edit_activity(bgc_id: str):
                 )
         if form.submit.data and form.validate():
             # return form.data # TODO: save to db
+            save_data(bgc_id, "Bio_activity", request.form)
             flash("Submitted activity information!")
             return redirect(url_for("edit_bgc", bgc_id=bgc_id))
     else:
         # prefill compounds
-        for c_name in ["a", "b"]:
-            form.activities.append_entry(data={"compound": c_name})
+        try:
+            products = products = next(
+                csv.reader(
+                    [MultiDict(read_data(bgc_id).get("Minimal")).get("products")],
+                    skipinitialspace=True,
+                )
+            )
+        except:
+            products = [""]
+
+        for product in products:
+            form.activities.append_entry(data={"compound": product})
     return render_template("biological_activity.html", bgc_id=bgc_id, form=form)
 
 
@@ -250,6 +269,7 @@ def edit_biosynth_class(bgc_id: str, b_class: str):
 
         # if no adds were triggered, validate and process data
         if form.validate():
+            save_data(bgc_id, f"BioSynth_{b_class}", request.form)
             flash(f"Submitted {b_class} biosynthesis information!")
             return redirect(url_for("edit_bgc", bgc_id=bgc_id))
             # return form.data
@@ -271,12 +291,14 @@ def edit_biosynth_class(bgc_id: str, b_class: str):
 def class_buttons(bgc_id: str):
 
     # grab classes for current bgc_id
-    classes = ["NRPS", "PKS", "Ribosomal", "Saccharide", "Terpene", "Other"]
+    classes = MultiDict(read_data(bgc_id).get("Minimal")).getlist("b_class")
+
+    if not classes:
+        classes = ["NRPS", "PKS", "Ribosomal", "Saccharide", "Terpene", "Other"]
     class_btns = ""
     for cls in classes:
-        class_btns += (
-            f"<a href='/edit/{bgc_id}/biosynth/{cls}'><button>{cls}</button><a>"
-        )
+        # class_btns += f"<a href='/edit/{bgc_id}/biosynth/{cls}'><button class='btn btn-light'>{cls}</button><a>"
+        class_btns += f"<a class='btn btn-light' style='margin: 5px' role='button' href='/edit/{bgc_id}/biosynth/{cls}'>{cls}<a>"
     return class_btns
 
 
@@ -289,6 +311,7 @@ def edit_tailoring(bgc_id: str):
     form = TailoringForm(request.form)
 
     if request.method == "POST" and form.validate():
+        save_data(bgc_id, "Tailoring", request.form)
         flash("Submitted tailoring information!")
         return redirect(url_for("edit_bgc", bgc_id=bgc_id))
 
@@ -304,6 +327,25 @@ def edit_tailoring(bgc_id: str):
 #     return render_template("forms_basic.html", form=request.form)
 
 
+@app.route("/add_evidence", methods=["POST"])
+def addd():
+    form = MinEntryForm(request.form)
+    form.evidence.append_entry()
+
+    return render_template_string(
+        """{% import 'macros.html' as m %}
+        <span>
+        {{m.simple_divsubform(field, deletebtn=True)}}</span>""",
+        field=form.evidence[-1],
+    )
+
+
+@app.route("/addtst", methods=["POST"])
+def addtest():
+    r = request
+    return ""
+
+
 @app.route("/delete", methods=["DELETE"])
 def delete():
     return ""
@@ -312,3 +354,33 @@ def delete():
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
     return render_template("base.html")
+
+
+def save_data(bgc_id: str, section_key: str, req_data: MultiDict):
+    """Append data to file containing all answers for one BGC"""
+    data = {section_key: [(k, v) for k in req_data for v in req_data.getlist(k)]}
+    existing_data = read_data(bgc_id)
+
+    existing_data.update(data)
+    with open(f"{bgc_id}_data.json", "w") as outf:
+        json.dump(existing_data, outf, sort_keys=True, indent=4)
+
+
+def read_data(bgc_id: str):
+    with open(f"{bgc_id}_data.json", "r") as inf:
+        if content := inf.read():
+            existing_data = json.loads(content)
+        else:
+            existing_data = {}
+    return existing_data
+
+
+def create_new_entry():
+    max_entry_id = 0
+    for filepath in Path(".").glob("new*"):
+        if (nr := int(filepath.stem[3:6])) > max_entry_id:
+            max_entry_id = nr
+    bgc_id = f"new{max_entry_id+1:0>3}"
+    Path(f"{bgc_id}_data.json").touch()
+
+    return bgc_id
