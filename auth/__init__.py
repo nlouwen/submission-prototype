5a9a9868e528ecd13a603530249b5aac62b5fce8
nlouwen <nicolouwen@hotmail.com>
2024-03-14 11:10:04 +0100

    chore: cleanup old files after refactor

--- auth/__init__.py
+++
@@ -1,4 +0,0 @@
-from .views import auth as auth_blueprint
-from .decorators import auth_role
-
-__all__ = ["auth_blueprint", "auth_role"]
--- extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_login import LoginManager
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-migrate = Migrate()
-login_manager = LoginManager()
-login_manager.login_view = "auth.login"
