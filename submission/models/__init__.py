60c565de59eef3758d58f497acde99c3e16ea8e3
Kai Blin <kblin@biosustain.dtu.dk>
2024-04-22 08:15:35 +0200

    chore: export the user models from the models module directly
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- submission/models/__init__.py
+++ submission/models/__init__.py
@@ -3,3 +3,5 @@ from .tokens import Token
 from .literature import Reference, EntryReference
 from .entries import Entry
 from .npatlas import NPAtlas
+
+__all__ = ["User", "Role", "UserRole", "UserInfo", "Token", "Reference", "EntryReference", "Entry", "NPAtlas"]
